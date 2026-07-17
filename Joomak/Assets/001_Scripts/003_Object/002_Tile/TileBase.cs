522457bc9356d09397c8bef92df16f05fad32e9f
swallow-smoke <teatimesucream@gmail.com>
2026-07-17 16:31:46 +0900

    Add | Tilemap Set

---
+++ Joomak/Assets/001_Scripts/003_Object/002_Tile/TileBase.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace _001_Scripts._003_Object._002_Tile
+{
+    [CreateAssetMenu(fileName = "create New Tile", menuName = "Tile/Create New Tile")]
+    public class TileBase : Tile
+    {
+        // 인스펙터에서 쓸 수 있게끔 타일 바로 설정
+    }
+}
